20775c01d5d217b331428f26aba13b8789e2d11c
Lance Ewing <lance.ewing@gmail.com>
2018-11-03 22:17:37 +0000

    Adding initial libgdx generated files for Jectrex.

---
+++ android/src/emu/jectrex/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package emu.jectrex;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import emu.jectrex.Jectrex;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Jectrex(), config);
+	}
+}
---
+++ desktop/src/emu/jectrex/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package emu.jectrex.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import emu.jectrex.Jectrex;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Jectrex(), config);
+	}
+}
